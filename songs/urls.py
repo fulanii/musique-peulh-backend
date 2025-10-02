578140d7f4d41c2362d4721dcf64350347aefd9a
Yassine Issoufou <68094236+fulanii@users.noreply.github.com>
2025-10-02 18:30:58 -0500

    fixed songs url naming to be more restful

--- songs/urls.py
+++ songs/urls.py
@@ -2,9 +2,9 @@ from django.urls import path, include
 from .views import AllSongs, SongUpload, GetSong, ArtistSongs, UploadedBySongs
 
 urlpatterns = [
-    path("song/upload/", SongUpload.as_view(), name="upload"),
-    path("all_songs/", AllSongs.as_view(), name="all_songs"),
-    path("song/title/<str:title>/", GetSong.as_view(), name="song"),
-    path("song/artist/<str:artist_name>/", ArtistSongs.as_view(), name="artist"),
-    path("song/upload_by/<str:uploaded_by>/", UploadedBySongs.as_view(), name="artist"),
+    path("songs/", AllSongs.as_view(), name="all_songs"),
+    path("songs/upload/", SongUpload.as_view(), name="upload"),
+    path("songs/titles/<str:title>/", GetSong.as_view(), name="song"),
+    path("songs/artists/<str:artist_name>/", ArtistSongs.as_view(), name="artist"),
+    path("songs/upload_by/<str:uploaded_by>/", UploadedBySongs.as_view(), name="upload_by"),
 ]
