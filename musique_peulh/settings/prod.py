31f1c5d924933359e22aef70204fec9222f06cc1
Yassine Issoufou <68094236+fulanii@users.noreply.github.com>
2025-10-08 22:46:18 -0500

    updated db and .env to use neon

--- musique_peulh/settings/prod.py
+++ musique_peulh/settings/prod.py
@@ -14,33 +14,20 @@ ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(",")
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": os.getenv("DB_NAME"),
-#         "USER": os.getenv("DB_USER"),
-#         "PASSWORD": os.getenv("DB_PASSWORD"),
-#         "HOST": os.getenv("DB_HOST"),
-#         "PORT": os.getenv("DB_PORT"),
-#     },
-#     "OPTIONS": {
-#         "sslmode": "require",
-#     },
-# }
-
-tmpPostgres = urlparse(os.getenv("DATABASE_URL"))
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': tmpPostgres.path.replace('/', ''),
-        'USER': tmpPostgres.username,
-        'PASSWORD': tmpPostgres.password,
-        'HOST': tmpPostgres.hostname,
-        'PORT': 5432,
-        'OPTIONS': dict(parse_qsl(tmpPostgres.query)),
-    }
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
+    },
+    "OPTIONS": {
+        "sslmode": os.getenv("DB_SSL_MODE", "require"),
+    },
 }
+
 
 # TODO: implement ✅ Security & HTTPS settings for production
 # --------------------------------------------------------------
