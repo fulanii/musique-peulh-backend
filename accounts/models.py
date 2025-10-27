35bd3b8d3f0aadb4110fe1a6ee3a657812dd9f90
Yassine Issoufou <68094236+fulanii@users.noreply.github.com>
2025-10-27 17:15:06 -0500

    refactored views and serializer changed var names, updated urls views name, added code expiration logic

--- accounts/models.py
+++ accounts/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import RegexValidator
+from django.utils import timezone
+from datetime import timedelta
 
 
 # Create your models here.
-class CustomUser(AbstractUser):
+class CustomUserModel(AbstractUser):
     email = models.EmailField(unique=True)
     username = models.CharField(
         unique=True,
@@ -24,16 +26,41 @@ class CustomUser(AbstractUser):
         return f"User with username: {self.username} and email: {self.email}"
 
 
-# TODO: Add expire logic later
-class EmailVerification(models.Model):
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+class EmailVerificationModel(models.Model):
+    user = models.ForeignKey(CustomUserModel, on_delete=models.CASCADE)
     code = models.IntegerField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    # expires_at = models.DateTimeField()
+    expires_at = models.DateTimeField()
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.expires_at = timezone.now() + timedelta(minutes=15)
+
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"Verification for {self.user.username}"
+
+    @property
+    def is_expired(self):
+        return timezone.now() >= self.expires_at
 
 
-class PasswordResetCode(models.Model):
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+class PasswordResetCodeModel(models.Model):
+    user = models.ForeignKey(CustomUserModel, on_delete=models.CASCADE)
     code = models.IntegerField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    # expires_at = models.DateTimeField()
+    expires_at = models.DateTimeField()
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.expires_at = timezone.now() + timedelta(minutes=15)
+
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"Verification for {self.user.username}"
+
+    @property
+    def is_expired(self):
+        return timezone.now() >= self.expires_at
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,32 +1,32 @@
 from django.urls import path, include
 from .views import (
-    RegisterUser,
-    LoginUser,
-    Verification,
-    GetUser,
-    GetUsers,
-    UpdateToAdmin,
-    DeleteUser,
-    ResendCode,
-    PasswordResetRequest,
-    PasswordReset,
+    RegisterUserView,
+    LoginUserView,
+    EmailVerificationView,
+    GetUserView,
+    GetUsersView,
+    UpdateToAdminView,
+    DeleteUserView,
+    ResendCodeView,
+    PasswordResetRequestView,
+    PasswordResetView,
 )
 
 urlpatterns = [
     # account related
-    path("auth/register/", RegisterUser.as_view(), name="register"),
-    path("auth/login/", LoginUser.as_view(), name="login"),
-    path("auth/verify-email/", Verification.as_view(), name="verify"),
-    path("auth/resend-verification/", ResendCode.as_view(), name="verify"),
+    path("auth/register/", RegisterUserView.as_view(), name="register"),
+    path("auth/login/", LoginUserView.as_view(), name="login"),
+    path("auth/verify-email/", EmailVerificationView.as_view(), name="verify-email"),
+    path("auth/resend-verification/", ResendCodeView.as_view(), name="resend-code"),
     path(
         "auth/reset-password-request/",
-        PasswordResetRequest.as_view(),
+        PasswordResetRequestView.as_view(),
         name="reset-request",
     ),
-    path("auth/reset-password/", PasswordReset.as_view(), name="reset"),
+    path("auth/reset-password/", PasswordResetView.as_view(), name="reset"),
     # user relates
-    path("auth/user/", GetUser.as_view(), name="user"),
-    path("auth/users/", GetUsers.as_view(), name="users"),
-    path("auth/user/admin/", UpdateToAdmin.as_view(), name="update_admin"),
-    path("auth/users/delete/<int:id>", DeleteUser.as_view(), name="users"),
+    path("auth/user/", GetUserView.as_view(), name="user"),
+    path("auth/users/", GetUsersView.as_view(), name="users"),
+    path("auth/user/admin/", UpdateToAdminView.as_view(), name="update_admin"),
+    path("auth/users/delete/<int:id>", DeleteUserView.as_view(), name="users"),
 ]
